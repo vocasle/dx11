ac63d8659d213cbee196a2ee523eab33bede269c
Nikita Elsakov <vocasle@outlook.com>
2022-04-10 13:52:56 +0300

    Set clear color to black

--- dx11/Renderer.cpp
+++ dx11/Renderer.cpp
@@ -135,11 +135,12 @@ void RClear(struct Renderer* renderer)
 	ID3D11RenderTargetView* rtv = renderer->DR->RenderTargetView;
 	ID3D11DepthStencilView* dsv = renderer->DR->DepthStencilView;
 
+	static constexpr float BLACK_COLOR[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
 	static const float CLEAR_COLOR[4] = { 0.392156899f, 0.584313750f, 0.929411829f, 1.000000000f };
 
 	ctx->Flush();
 
-	ctx->ClearRenderTargetView(rtv, CLEAR_COLOR);
+	ctx->ClearRenderTargetView(rtv, BLACK_COLOR);
 	ctx->ClearDepthStencilView(dsv, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
 	ctx->OMSetRenderTargets(1, &rtv, dsv);
 	ctx->RSSetViewports(1, &renderer->DR->ScreenViewport);
