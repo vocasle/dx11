0f86d76b2cb3eee2671227544a56919115933117
Nikita Elsakov <vocasle@gmail.com>
2022-04-21 22:21:17 +0300

    Allow to bind actors to CubeMap

--- reflections/CubeMap.cpp
+++ reflections/CubeMap.cpp
@@ -62,6 +62,36 @@ void CubeMap::LoadCubeMap(ID3D11Device* device, const char* filepath)
 	CreateSampler(device);
 }
 
+void CubeMap::SetActor(const Actor& actor)
+{
+	m_cube = actor;
+}
+
+void CubeMap::Draw(ID3D11DeviceContext* ctx)
+{
+	uint32_t strides =  sizeof(Vertex);
+	uint32_t offsets = 0;
+
+	ID3D11Buffer* vb = m_cube.GetVertexBuffer();
+	ctx->IASetVertexBuffers(0, 1, &vb, &strides, &offsets);
+	ctx->IASetIndexBuffer(m_cube.GetIndexBuffer(), DXGI_FORMAT_R32_UINT, 0);
+
+	// Erase the translation component to avoid skybox jitter caused by camera movement
+	Mat4X4 V = m_camera->GetViewMat();
+	V.V[3] = { 0.0f, 0.0f, 0.0f, 1.0f };
+	//skyEffect.SetWorldMatrix(XMMatrixIdentity());
+	//skyEffect.SetViewMatrix(V);
+	//skyEffect.SetProjMatrix(camera.GetProjXM());
+	//skyEffect.SetTextureCube(m_pTextureCubeSRV.Get());
+	//skyEffect.Apply(deviceContext);
+	ctx->DrawIndexed(m_cube.GetNumIndices(), 0, 0);
+}
+
+void CubeMap::SetCamera(Camera* camera)
+{
+	m_camera = camera;
+}
+
 void CubeMap::CreateSampler(ID3D11Device* device)
 {
 	D3D11_SAMPLER_DESC desc = {};
--- reflections/CubeMap.h
+++ reflections/CubeMap.h
@@ -2,6 +2,12 @@
 
 #include <wrl/client.h>
 #include <d3d11.h>
+#include <vector>
+#include <cstdint>
+
+#include "Math.h"
+#include "Actor.h"
+#include "Camera.h"
 
 class CubeMap
 {
@@ -12,14 +18,19 @@ public:
 	void LoadCubeMap(ID3D11Device* device, const char* filepath);
 	ID3D11ShaderResourceView* GetCubeMap() const { return m_cubeMap.Get(); }
 	ID3D11SamplerState* GetCubeMapSampler() const { return m_sampler.Get(); }
+	void SetActor(const Actor& actor);
+	void Draw(ID3D11DeviceContext* ctx);
+	void SetCamera(Camera* camera);
 
 private:
 	void CreateSampler(ID3D11Device* device);
 
-
 	template <typename T>
 	using ComPtr = Microsoft::WRL::ComPtr<T>;
 
-	ComPtr<ID3D11ShaderResourceView>	m_cubeMap;
-	ComPtr<ID3D11SamplerState>			m_sampler;
+	ComPtr<ID3D11ShaderResourceView> m_cubeMap;
+	ComPtr<ID3D11SamplerState> m_sampler;
+
+	Actor m_cube;
+	Camera* m_camera;
 };
