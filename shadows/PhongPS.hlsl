75c554b847c411a25ad4f28a5a26a486996d05f2
Nikita Elsakov <vocasle@gmail.com>
2022-04-21 11:00:59 +0300

    Use shadow from shadow map in light calculations

--- shadows/PhongPS.hlsl
+++ shadows/PhongPS.hlsl
@@ -11,8 +11,22 @@ float4 main(VSOut In) : SV_TARGET
 	const float3 normal = normalize(In.NormalW);
 	const float3 toEye = normalize(cameraPosW - In.PosW);
 
-	float4 resultColor = float4(0.0f, 0.0f, 0.0f, 0.0f);
-	resultColor += ComputeDirectionalLight(mat, dirLight, normal, toEye);
+	Material material;
+	material.Ambient = float4(0.0f, 0.0f, 0.0f, 0.0f);
+	material.Diffuse = float4(0.0f, 0.0f, 0.0f, 0.0f);
+	material.Specular = float4(0.0f, 0.0f, 0.0f, 0.0f);
+
+	// Only the first light casts a shadow
+	float shadow[] = { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f };
+
+	Material part;
+	part = ComputeDirectionalLight(mat, dirLight, normal, toEye);
+	shadow[0] = CalcShadowFactor(shadowSampler, shadowTexture, In.ShadowPosH);
+
+	material.Ambient = part.Ambient;
+	material.Diffuse = part.Diffuse * shadow[0];
+	material.Specular = part.Specular * shadow[0];
+
 
 	//for (int i = 0; i < 4; ++i)
 	//{
@@ -24,5 +38,5 @@ float4 main(VSOut In) : SV_TARGET
 	//	resultColor += ComputeSpotLight(mat, spotLights[i], In.PosW, normal, toEye);
 	//}
 
-	return saturate(resultColor);
+	return saturate(material.Ambient + material.Diffuse + material.Specular);
 }
