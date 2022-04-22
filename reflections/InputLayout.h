ae45954e1c91b8c62c7c2e97822726aa12929e66
Nikita Elsakov <vocasle@gmail.com>
2022-04-22 11:04:44 +0300

    Add InputLayout class

---
+++ reflections/InputLayout.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <d3d11.h>
+#include <wrl/client.h>
+
+class InputLayout
+{
+public:
+	InputLayout();
+	void CreateDefaultLayout(ID3D11Device* device, unsigned char* bytes, size_t bufferSize);
+	void CreateSkyLayout(ID3D11Device* device, unsigned char* bytes, size_t bufferSize);
+
+	ID3D11InputLayout* GetDefaultLayout() const { return m_defaultLayout.Get(); }
+	ID3D11InputLayout* GetSkyLayout() const { return m_skyLayout.Get(); }
+
+private:
+
+	Microsoft::WRL::ComPtr<ID3D11InputLayout> m_defaultLayout;
+	Microsoft::WRL::ComPtr<ID3D11InputLayout> m_skyLayout;
+};
