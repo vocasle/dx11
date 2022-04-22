19d19f6d21364037bbe9963fe3dc0bf5dcf582dc
Nikita Elsakov <vocasle@gmail.com>
2022-04-22 12:00:03 +0300

    Change default sampler's max anisotropy

--- reflections/Game.cpp
+++ reflections/Game.cpp
@@ -68,7 +68,7 @@ void Game::CreateDefaultSampler()
 	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc.ComparisonFunc = D3D11_COMPARISON_GREATER;
-	samplerDesc.MaxAnisotropy = 16;
+	samplerDesc.MaxAnisotropy = D3D11_REQ_MAXANISOTROPY;
 	samplerDesc.MipLODBias = 0.0f;
 	samplerDesc.MinLOD = 0;
 	samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
