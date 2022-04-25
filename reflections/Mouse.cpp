ebf3e6a4a64668168d56b211e31450eed9311e49
Nikita Elsakov <vocasle@gmail.com>
2022-04-25 16:52:18 +0300

    Fix bug with auto mouse movement
    This bug was caused by fast right button click with following release of the button

--- reflections/Mouse.cpp
+++ reflections/Mouse.cpp
@@ -41,7 +41,6 @@ void Mouse::OnMouseMove(uint32_t message, WPARAM wParam, LPARAM lParam)
 {
 	if (m_RightBtnState)
 	{
-		static uint32_t firstLaunch = 1;
 		MousePos.X = GET_X_LPARAM(lParam);
 		MousePos.Y = GET_Y_LPARAM(lParam);
 		POINT pos = {};
@@ -49,12 +48,6 @@ void Mouse::OnMouseMove(uint32_t message, WPARAM wParam, LPARAM lParam)
 		MousePos.X = pos.x;
 		MousePos.Y = pos.y;
 		SetCursorPos((int32_t)WinSize.X / 2, (int32_t)WinSize.Y / 2);
-		if (firstLaunch)
-		{
-			firstLaunch = 0;
-			MousePos.X = WinSize.X / 2;
-			MousePos.Y = WinSize.Y / 2;
-		}
 	}
 }
 
@@ -98,10 +91,10 @@ Vec2D Mouse::GetMouseDelta()
 {
 
 	Vec2D delta = {};
-	delta.X = WinSize.X / 2.0f - MousePos.X;
-	delta.Y = WinSize.Y / 2.0f - MousePos.Y;
-
-	//UtilsDebugPrint("delta={%f %f}\n", delta.X, delta.Y);
-
+	if (m_RightBtnState)
+	{
+		delta.X = WinSize.X / 2.0f - MousePos.X;
+		delta.Y = WinSize.Y / 2.0f - MousePos.Y;
+	}
 	return delta;
 }
