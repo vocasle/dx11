4c1655853a2d5aa8e662fef7629b966bd53a63b8
Nikita Elsakov <vocasle@gmail.com>
2022-05-05 01:26:38 +0300

    Positions should have 4th component equal to 1.0

--- reflections/VertexShader.hlsl
+++ reflections/VertexShader.hlsl
@@ -8,8 +8,8 @@ VSOut main(VSIn In)
 	Out.PosH = mul(pvw, float4(In.Pos, 1.0f));
 	Out.TexCoords = In.TexCoords;
 	Out.NormalW = mul(worldInvTranspose, float4(In.Normal, 0.0f)).xyz;
-	Out.PosW = mul(worldInvTranspose, float4(In.Pos, 0.0f)).xyz;
-	Out.ShadowPosH = mul(shadowTransform, float4(In.Pos, 0.0f));
+	Out.PosW = mul(worldInvTranspose, float4(In.Pos, 1.0f)).xyz;
+	Out.ShadowPosH = mul(shadowTransform, float4(In.Pos, 1.0f));
 	Out.TangentW = mul(worldInvTranspose, float4(In.Tangent, 0.0f));
 	Out.BitangentW = mul(worldInvTranspose, float4(In.Bitangent, 0.0f));
 	return Out;
