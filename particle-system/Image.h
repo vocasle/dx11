cacf60495c1ab356e0d3777d96147b27bc79ef71
Nikita Elsakov <vocasle@outlook.com>
2022-05-22 18:35:14 +0300

    Add Image wrapper around stb_image

---
+++ particle-system/Image.h
@@ -0,0 +1,32 @@
+#include <string>
+#include <cstdint>
+
+#include "stb_image.h"
+#include "Utils.h"
+
+class Image
+{
+public:
+	Image(const std::string& filepath): m_bytes{nullptr}, m_width{0}, m_height{0}, m_channels{0}
+	{
+		m_bytes = stbi_load(filepath.c_str(), &m_width, &m_height, &m_channels, DESIRED_CHANNELS);
+
+		if (!m_bytes)
+		{
+			UtilsFatalError("Failed to load image from %s\n", filepath.c_str());
+		}
+	}
+
+	~Image()
+	{
+		stbi_image_free(m_bytes);
+	}
+
+private:
+	uint8_t* m_bytes;
+	int m_width;
+	int m_height;
+	int m_channels;
+
+	static const int DESIRED_CHANNELS = 4; // for RGBA
+};
