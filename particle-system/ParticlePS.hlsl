9cf2b52dc51ef1490b46b209434d5bf8a9d44edd
Nikita Elsakov <vocasle@outlook.com>
2022-05-28 00:11:50 +0300

    Remove debug red overlay

--- particle-system/ParticlePS.hlsl
+++ particle-system/ParticlePS.hlsl
@@ -2,5 +2,5 @@
 
 float4 main(PSIn pin) : SV_TARGET
 {
-	return diffuseTexture.Sample(defaultSampler, pin.TexCoords) + float4(1.0f, 0.0f, 0.0f, 0.0f);
+	return diffuseTexture.Sample(defaultSampler, pin.TexCoords);
 }
