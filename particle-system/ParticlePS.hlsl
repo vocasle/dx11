7e7fc63a2f84b682524212973ec77298bdaa9d92
Nikita Elsakov <vocasle@outlook.com>
2022-05-28 01:05:58 +0300

    Some primitive ideas on how to change color over time

--- particle-system/ParticlePS.hlsl
+++ particle-system/ParticlePS.hlsl
@@ -2,5 +2,12 @@
 
 float4 main(PSIn pin) : SV_TARGET
 {
-	return diffuseTexture.Sample(defaultSampler, pin.TexCoords);
+	const float x = pin.Lifespan / 10.0f;
+	float3 col = float3(1.0f / (x + 1.0f) , x / 2.0f, x);
+	float4 sampled = diffuseTexture.Sample(defaultSampler, pin.TexCoords);
+	if (length(sampled.rgb) > 0.01f)
+	{
+		sampled.rgb += col;
+	}
+	return sampled;
 }
