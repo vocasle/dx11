0ef5ef2f6ec1fdd39c1feae480a1e749c1b4b4c6
Nikita Elsakov <vocasle@outlook.com>
2022-05-28 20:21:13 +0300

    Add soft-shadows project

---
+++ soft-shadows/PhongPS.hlsl
@@ -0,0 +1,57 @@
+#include "Common.hlsli"
+
+static const float4 AMBIENT = float4(0.1f, 0.1f, 0.1f, 1.0f);
+
+float4 main(VSOut In) : SV_TARGET
+{
+	const float4 diffuseSampled = diffuseTexture.Sample(defaultSampler, In.TexCoords);
+	const float4 specularSampled = specularTexture.Sample(defaultSampler, In.TexCoords);
+	const float4 glossSampled = glossTexture.Sample(defaultSampler, In.TexCoords);
+	const float3 normalSampled = normalTexture.Sample(defaultSampler, In.TexCoords).xyz;
+	float3 normal = NormalSampleToWorldSpace(normalSampled, normalize(In.NormalW), normalize(In.TangentW));
+	normal = normalize(normal);
+	
+	const float3 viewDir = normalize(cameraPosW - In.PosW);
+
+	LightIntensity intensities[MAX_LIGHTS];
+	intensities[0] = DirectionalLightIntensity(dirLight, normal, viewDir);
+
+	const uint numPointLights = 4;
+	for (uint i = 0; i < numPointLights; ++i)
+	{
+		intensities[1 + i] = PointLightIntensity(pointLights[i], normal, In.PosW, viewDir);
+	}
+
+	intensities[5] = SpotLightIntensity(spotLights[0], normal, In.PosW, viewDir);
+	float shadows[MAX_LIGHTS] = {1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f};
+	//shadows[0] = CalcShadowFactor(shadowSampler, shadowTexture, In.ShadowPosH);
+
+	//const float diff = dot(normalize(normal), normalize(dirLight.Position));
+	//return float4(diff, diff, diff, 1.0f);
+
+	const float4 emissive = ZERO_VEC4;
+	const float4 emissiveSampled = ZERO_VEC4;
+	float4 fragmentColor = BlinnPhong(
+		emissive,
+		emissiveSampled,
+		material.Diffuse,
+		diffuseSampled,
+		AMBIENT,
+		specularSampled,
+		glossSampled,
+		material.Specular.a,
+		normal,
+		intensities,
+		shadows,
+		1
+	);
+
+	// calculate reflections
+	float3 incident = -viewDir;
+	float3 reflVector = reflect(incident, normal);
+	float4 reflColor = envTexture.Sample(defaultSampler, reflVector);
+
+	fragmentColor += reflColor * material.Reflection;
+
+	return fragmentColor;
+}
