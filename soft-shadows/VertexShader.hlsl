e52182fd1e0bda7ed7465bed5b2b9d6e6ba14dc9
Nikita Elsakov <vocasle@outlook.com>
2022-06-02 23:06:44 +0300

    Replace for now worldInvTranspose with world

--- soft-shadows/VertexShader.hlsl
+++ soft-shadows/VertexShader.hlsl
@@ -7,10 +7,11 @@ VSOut main(VSIn In)
 	pvw = mul(pvw, world);
 	Out.PosH = mul(pvw, float4(In.Pos, 1.0f));
 	Out.TexCoords = In.TexCoords;
-	Out.NormalW = mul(worldInvTranspose, float4(In.Normal, 0.0f)).xyz;
+	// TODO: Check matrix inverse code. It generates invalid matrix
+	Out.NormalW = mul(world, float4(In.Normal, 0.0f)).xyz;
 	Out.PosW = mul(world, float4(In.Pos, 1.0f)).xyz;
 	Out.ShadowPosH = mul(shadowTransform, float4(In.Pos, 1.0f));
-	Out.TangentW = mul(worldInvTranspose, float4(In.Tangent, 0.0f)).xyz;
-	Out.BitangentW = mul(worldInvTranspose, float4(In.Bitangent, 0.0f)).xyz;
+	Out.TangentW = mul(world, float4(In.Tangent, 0.0f)).xyz;
+	Out.BitangentW = mul(world, float4(In.Bitangent, 0.0f)).xyz;
 	return Out;
 }
