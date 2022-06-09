29a04a15ad2df54eafe3af611569396cbac11825
Nikita Elsakov <vocasle@gmail.com>
2022-06-09 10:17:08 +0300

    Fix posH type

--- soft-shadows/shader/ShadowVS.hlsl
+++ soft-shadows/shader/ShadowVS.hlsl
@@ -1,11 +1,12 @@
 #include "Common.hlsli"
 
+
 VSOut main(VSIn vin)
 {
 	//float4x4 toLightSpace = mul(shadowTransform, world);
 	//float4 posH = mul(toLightSpace, float4(pos, 1.0f));
 	VSOut vout = EMPTY_VSOUT;
-	float posH = mul(proj, float4(vin.Pos, 1.0f));
+	float4 posH = mul(float4(vin.Pos, 1.0f), proj);
 	posH = mul(view, posH);
 	posH = mul(world, posH);
 	vout.PosH = posH;
