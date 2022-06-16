80ab96bab11357516a9bdd61e3663a92f4ac7cc0
Nikita Elsakov <vocasle@gmail.com>
2022-06-16 10:22:06 +0300

    Do not run executable if build failed

--- build_run.bat
+++ build_run.bat
@@ -1,3 +1,11 @@
-cmake --build build --target %TARGET%
-copy build\compile_commands.json .
-build\%TARGET%\%TARGET%.exe
+@echo off
+echo Compiling %TARGET% executable
+CALL cmake --build build --target %TARGET%
+IF %ERRORLEVEL% NEQ 0 (
+	echo Failed to compile %TARGET%
+	EXIT 1
+)
+echo Copying compile_commands.json
+CALL copy build\compile_commands.json .
+echo Invoking %TARGET%.exe
+CALL build\%TARGET%\%TARGET%.exe
