72a1371055359b651d9df04e60fe862c44025d12
Nikita Elsakov <vocasle@gmail.com>
2022-06-16 09:12:41 +0300

    Add default ctor to Timer

--- utils/Timer.h
+++ utils/Timer.h
@@ -9,37 +9,43 @@
 
 #define MAX_DELTA_TIME 1000
 
-typedef struct Timer
+struct Timer
 {
-	double DeltaMillis;
-	LARGE_INTEGER StartTime;
-	LARGE_INTEGER EndTime;
-	LARGE_INTEGER Frequency;
-	LARGE_INTEGER ElapsedMicroseconds;
-} Timer;
+    Timer()
+        : DeltaMillis(0.0f), StartTime(), EndTime(), Frequency(),
+          ElapsedMicroseconds()
+    {
+    }
 
-inline void TimerInitialize(Timer* timer)
+    double DeltaMillis;
+    LARGE_INTEGER StartTime;
+    LARGE_INTEGER EndTime;
+    LARGE_INTEGER Frequency;
+    LARGE_INTEGER ElapsedMicroseconds;
+};
+
+inline void TimerInitialize(Timer *timer)
 {
-	timer->DeltaMillis = 0.0;
-	memset(timer, 0, sizeof(Timer));
-	QueryPerformanceFrequency(&timer->Frequency);
+    timer->DeltaMillis = 0.0;
+    memset(timer, 0, sizeof(Timer));
+    QueryPerformanceFrequency(&timer->Frequency);
 }
 
-inline void TimerTick(Timer* timer)
+inline void TimerTick(Timer *timer)
 {
-	if (!QueryPerformanceCounter(&timer->EndTime))
-	{
-		OutputDebugStringA("ERROR: Failed to query performance counter\n");
-		ExitProcess(EXIT_FAILURE);
-	}
-	
-	uint64_t timeDelta = timer->EndTime.QuadPart - timer->StartTime.QuadPart;
+    if (!QueryPerformanceCounter(&timer->EndTime))
+    {
+        OutputDebugStringA("ERROR: Failed to query performance counter\n");
+        ExitProcess(EXIT_FAILURE);
+    }
 
-	timeDelta *= 1000000;
-	timeDelta /= timer->Frequency.QuadPart;
+    uint64_t timeDelta = timer->EndTime.QuadPart - timer->StartTime.QuadPart;
 
-	timer->DeltaMillis = (double)timeDelta / 1000.0; // convert to ms
-	if (timer->DeltaMillis > MAX_DELTA_TIME)
-		timer->DeltaMillis = MAX_DELTA_TIME;
-	timer->StartTime = timer->EndTime;
+    timeDelta *= 1000000;
+    timeDelta /= timer->Frequency.QuadPart;
+
+    timer->DeltaMillis = (double)timeDelta / 1000.0; // convert to ms
+    if (timer->DeltaMillis > MAX_DELTA_TIME)
+        timer->DeltaMillis = MAX_DELTA_TIME;
+    timer->StartTime = timer->EndTime;
 }
