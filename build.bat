fca0c020148d10aad2c91c49be7a77b9e37b06a9
Nikita Elsakov <vocasle@gmail.com>
2022-06-19 11:57:02 +0300

    Generate debug build by default

---
+++ build.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Compiling %TARGET% executable
+CALL cmake --build build --target %TARGET%
+IF %ERRORLEVEL% NEQ 0 (
+	echo Failed to compile %TARGET%
+	EXIT 1
+)
+echo Copying compile_commands.json
+CALL copy build\compile_commands.json .
