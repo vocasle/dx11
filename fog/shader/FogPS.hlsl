dc7ccbc9ae9a276fdd9ab47d9825d977003d837d
Nikita Elsakov <vocasle@gmail.com>
2022-07-12 23:12:49 +0300

    Try to convert depth from depth map to world space. Not working edition

--- fog/shader/FogPS.hlsl
+++ fog/shader/FogPS.hlsl
@@ -1,15 +1,36 @@
 #include "Fog.hlsli"
 
+// this is supposed to get the world position from the depth buffer
+float3 WorldPosFromDepth(float depth,
+    float2 texCoord,
+    float4x4 projMatrixInv,
+    float4x4 viewMatrixInv)
+{
+    float z = depth * 2.0 - 1.0;
+
+    float4 clipSpacePosition = float4(texCoord * 2.0 - 1.0, z, 1.0);
+    float4 viewSpacePosition = mul(projMatrixInv, clipSpacePosition);
+
+    // Perspective division
+    viewSpacePosition /= viewSpacePosition.w;
+
+    float4 worldSpacePosition = mul(viewMatrixInv, viewSpacePosition);
+
+    return worldSpacePosition.xyz;
+}
+
+
 float4 main(PSIn pin) : SV_TARGET
 {
-	float fogFactor = clamp((fogEnd - pin.PosH.z) / (fogEnd - fogStart), 0, 1);
+    static const float MAX_DEPTH = 1000;
 
-//	return float4(fogFactor, fogFactor, fogFactor, 1.0f);
+    float4 depth = depthBuffer.Sample(defaultSampler, pin.TexCoord);
+//    return float4(depth.xxx, 1.0f);
+
+    float d = WorldPosFromDepth(depth.x, pin.TexCoord, projInverse, viewInverse).x;
+
 	float4 color =  backBuffer.Sample(defaultSampler, pin.TexCoord);
-
-	float f = pow(2.7f, -(1.0f * pin.PosH.z));
-	color = f * color + (1 - f) * fogColor;
-
-//	color.rgb = lerp(color.rgb, (fogColor * color.a).rgb, fogFactor);
+	float fogFactor = clamp((fogEnd - d) / (fogEnd - fogStart), 0, 1);
+    color = fogFactor * color + (1.0f - fogFactor) * fogColor;
 	return color;
 }
