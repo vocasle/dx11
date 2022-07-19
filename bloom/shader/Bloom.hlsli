ca365f34a1d9431c86736c37c752a11e7caf3e81
Nikita Elsakov <vocasle@gmail.com>
2022-07-19 15:58:29 +0300

    Implement naive bloom

--- bloom/shader/Bloom.hlsli
+++ bloom/shader/Bloom.hlsli
@@ -9,7 +9,7 @@ float4 saturatePixel(float4 color)
     const float3 threshold = float3(0.2126, 0.7152, 0.0722);
     const float brightess = dot(color.rgb, threshold);
 
-    if (brightess > 0.5f)
+    if (brightess > 0.7f)
     {
         return float4(color.rgb, 1.0f);
     }
--- bloom/shader/BloomPS.hlsl
+++ bloom/shader/BloomPS.hlsl
@@ -21,13 +21,12 @@ float4 main(PSIn pin) : SV_TARGET
     float4 brightness = brightessBuffer.Sample(defaultSampler, pin.TexCoord);
     float4 color = backBuffer.Sample(defaultSampler, pin.TexCoord);
 
-
     const float dx = 1.0f / width;
     const float dy = 1.0f / height;
 
     float4 result = {0,0,0,0};
 
-    static const int r = 3;
+    static const int r = 10;
     static const float divisor = (2 * r + 1) * (2 * r + 1);
 
     for (int y = -r; y <= r; ++y)
@@ -35,9 +34,11 @@ float4 main(PSIn pin) : SV_TARGET
         for (int x = -r; x <= r; ++x)
         {
             const float2 tc = pin.TexCoord + float2(dx * x, dy * y);
-            result += backBuffer.Sample(defaultSampler, tc);
+            result += brightessBuffer.Sample(defaultSampler, tc);
         }
     }
 
-    return result / divisor;
+    float4 blurColor = result / divisor;
+
+    return color + blurColor;
 }
