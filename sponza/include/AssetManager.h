c0e8d1a7233dde20dc732700e5d12a5b57a64ac6
Nikita Elsakov <vocasle@gmail.com>
2022-07-31 14:02:57 +0300

    Move model loading to AssetManager class

--- sponza/include/AssetManager.h
+++ sponza/include/AssetManager.h
@@ -5,6 +5,7 @@
 
 #include <string>
 #include <unordered_map>
+#include "ModelLoader.h"
 
 class AssetManager
 {
@@ -12,9 +13,12 @@ public:
     AssetManager(DeviceResources& deviceResources);
     Texture* LoadTexture(const std::string& path);
     Texture* GetTexture(const std::string& path);
+    std::vector<Mesh> LoadModel(const std::string& path);
+    Texture* LoadEmbeddedTexture(const std::string& path);
 
 private:
 
     DeviceResources* m_deviceResources;
     std::unordered_map<std::string, Texture> m_textures;
+    ModelLoader m_modelLoader;
 };
--- sponza/src/AssetManager.cpp
+++ sponza/src/AssetManager.cpp
@@ -1,17 +1,22 @@
 #include "AssetManager.h"
 
+#include <assimp/scene.h>
 #include <d3d11.h>
+
+#include "Texture.h"
+#include "Utils.h"
 
 AssetManager::AssetManager(DeviceResources &deviceResources)
     : m_deviceResources(&deviceResources) {
 }
 
-Texture*
+Texture *
 AssetManager::LoadTexture(const std::string &path) {
-    if (Texture* tex = GetTexture(path))
+    if (Texture *tex = GetTexture(path))
         return tex;
 
-    m_textures.insert(std::make_pair(path, Texture(path, m_deviceResources->GetDevice())));
+    m_textures.insert(
+        std::make_pair(path, Texture(path, m_deviceResources->GetDevice())));
     return &m_textures.at(path);
 }
 
@@ -20,5 +25,71 @@ AssetManager::GetTexture(const std::string &path) {
     auto it = m_textures.find(path);
     if (it != std::end(m_textures))
         return &it->second;
+    return nullptr;
+}
+std::vector<Mesh>
+AssetManager::LoadModel(const std::string &path) {
+    std::vector<Mesh> meshes = m_modelLoader.Load(path);
+
+    for (Mesh &mesh : meshes) {
+        mesh.CreateDeviceDependentResources(m_deviceResources->GetDevice());
+
+        for (const TextureInfo &ti : mesh.GetTextures()) {
+            if (ti.StorageType == TextureStorageType::Detached) {
+                LoadTexture(ti.Path);
+            } else if (ti.StorageType == TextureStorageType::Embedded) {
+                LoadEmbeddedTexture(ti.Path);
+            }
+        }
+    }
+    m_modelLoader.FreeScene();
+    return meshes;
+}
+Texture *
+AssetManager::LoadEmbeddedTexture(const std::string &path) {
+    const aiScene *scene = m_modelLoader.GetScenePtr();
+    if (!scene) {
+        UtilsDebugPrint(
+            "ERROR: Failed to load embedded texture %s because scene is not "
+            "available\n",
+            path.c_str());
+        return nullptr;
+    }
+
+    for (unsigned int i = 0; i < scene->mNumTextures; ++i) {
+        const aiTexture *tex = scene->mTextures[i];
+        if (std::strcmp(tex->mFilename.C_Str(), path.c_str()) == 0) {
+            if (tex->mHeight != 0) {
+                m_textures.insert(
+                    std::make_pair(path,
+                                   Texture(DXGI_FORMAT_B8G8R8A8_UNORM,
+                                           static_cast<int>(tex->mWidth),
+                                           static_cast<int>(tex->mHeight),
+                                           tex->pcData,
+                                           tex->mWidth * 4,
+                                           tex->mWidth * tex->mHeight * 4,
+                                           m_deviceResources->GetDevice())));
+
+                return &m_textures.at(path);
+            }
+
+            //    // mHeight is 0, so try to load a compressed texture of mWidth bytes
+            //    const size_t size = embeddedTexture->mWidth;
+            //
+            //    hr = CreateWICTextureFromMemory(
+            //        dev_,
+            //        devcon_,
+            //        reinterpret_cast<const unsigned char *>(embeddedTexture->pcData),
+            //        size,
+            //        nullptr,
+            //        &texture);
+            //    if (FAILED(hr)) 		MessageBox(hwnd_, "Texture couldn't be created
+            // from memory!", "Error!", MB_ICONERROR | MB_OK);
+            UtilsDebugPrint(
+                "ERROR: Failed to load embedded texture %s because height is "
+                "0\n",
+                path.c_str());
+        }
+    }
     return nullptr;
 }
