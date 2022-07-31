bcea87ff2712fdb523ec66702c2f0dd4b148f752
Nikita Elsakov <vocasle@gmail.com>
2022-07-31 13:04:32 +0300

    Implement ctors for Texture class

--- common/include/Texture.h
+++ common/include/Texture.h
@@ -7,38 +7,39 @@
 #include <string>
 
 class Texture {
-    public:
-	Texture(DXGI_FORMAT format, int width, int height,
-		ID3D11Device *device);
-	Texture(const std::string &filepath, ID3D11Device *device);
-	void GenerateMips(ID3D11DeviceContext *context);
+public:
+    Texture(DXGI_FORMAT format, int width, int height, ID3D11Device *device);
+    Texture(const std::string &filepath, ID3D11Device *device);
+    Texture(const Texture &rhs);
+    Texture &operator=(const Texture &rhs);
+    void GenerateMips(ID3D11DeviceContext *context);
 
-	ID3D11ShaderResourceView *GetSRV() const
-	{
-		return mSRV.Get();
-	}
-	ID3D11RenderTargetView *GetRTV() const
-	{
-		return mRTV.Get();
-	}
-	ID3D11DepthStencilView *GetDSV() const
-	{
-		return mDSV.Get();
-	}
-	ID3D11ShaderResourceView *GetDepthSRV() const
-	{
-		return mDepthSRV.Get();
-	}
+    [[nodiscard]] ID3D11ShaderResourceView *
+    GetSRV() const {
+        return mSRV.Get();
+    }
+    [[nodiscard]] ID3D11RenderTargetView *
+    GetRTV() const {
+        return mRTV.Get();
+    }
+    [[nodiscard]] ID3D11DepthStencilView *
+    GetDSV() const {
+        return mDSV.Get();
+    }
+    [[nodiscard]] ID3D11ShaderResourceView *
+    GetDepthSRV() const {
+        return mDepthSRV.Get();
+    }
 
-    private:
-	Microsoft::WRL::ComPtr<ID3D11Texture2D> mTexture;
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> mSRV;
-	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> mRTV;
-	Microsoft::WRL::ComPtr<ID3D11DepthStencilView> mDSV;
-	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> mDepthSRV;
-	Microsoft::WRL::ComPtr<ID3D11Texture2D> mDepthTexture;
+private:
+    Microsoft::WRL::ComPtr<ID3D11Texture2D> mTexture;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> mSRV;
+    Microsoft::WRL::ComPtr<ID3D11RenderTargetView> mRTV;
+    Microsoft::WRL::ComPtr<ID3D11DepthStencilView> mDSV;
+    Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> mDepthSRV;
+    Microsoft::WRL::ComPtr<ID3D11Texture2D> mDepthTexture;
 
-	int mWidth;
-	int mHeight;
-	DXGI_FORMAT mFormat;
+    int mWidth;
+    int mHeight;
+    DXGI_FORMAT mFormat;
 };
