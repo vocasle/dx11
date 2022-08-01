eee1495b87691828c5262851b1a999a34be9cc19
Nikita Elsakov <vocasle@gmail.com>
2022-08-01 21:32:45 +0300

    Insert padding in case alignment is incorrect

--- common/include/DynamicConstBuffer.h
+++ common/include/DynamicConstBuffer.h
@@ -5,12 +5,13 @@
 
 #include <cassert>
 #include <functional>
+#include <numeric>
 #include <string>
 #include <unordered_map>
 #include <vector>
 
-#include "Utils.h"
 #include "DeviceResources.h"
+#include "Utils.h"
 
 enum class NodeType {
     Struct,
@@ -99,14 +100,9 @@ class DynamicConstBuffer {
 public:
     DynamicConstBuffer(const DynamicConstBufferDesc &desc,
                        DeviceResources &deviceResources)
-        : DynamicConstBuffer(desc) {
-		CreateConstantBuffer(deviceResources.GetDevice());
-		UpdateConstantBuffer(deviceResources.GetDeviceContext());
-    }
-
-    DynamicConstBuffer(const DynamicConstBufferDesc &inDesc) {
+        : mDeviceResources(&deviceResources) {
         size_t sz = 0;
-        for (auto &node : inDesc.GetNodes()) {
+        for (auto &node : desc.GetNodes()) {
             auto visitor = [&sz](const Node &node) -> void {
                 if (node.Type != NodeType::Array &&
                     node.Type != NodeType::Struct) {
@@ -115,10 +111,15 @@ public:
             };
             node.Visit(visitor);
         }
+        if (sz % 16 != 0) {
+            UtilsDebugPrint(
+                "WARN: Invalid alignment for cbuffer. Adding padding\n");
+            sz = static_cast<size_t>(std::ceil(sz / 16.0f) * 16);
+        }
         mBytes.resize(sz);
 
         size_t offset = 0;
-        for (auto &node : inDesc.GetNodes()) {
+        for (auto &node : desc.GetNodes()) {
             auto visitor = [this, &offset](const Node &node) -> void {
                 if (node.Type != NodeType::Struct &&
                     node.Type != NodeType::Array) {
@@ -130,9 +131,12 @@ public:
             };
             node.Visit(visitor);
         }
+
+        CreateConstantBuffer();
+        UpdateConstantBuffer();
     }
 
-    ID3D11Buffer *
+    [[nodiscard]] ID3D11Buffer *
     Get() const {
         return mBuffer.Get();
     }
@@ -178,19 +182,19 @@ public:
     }
 
     void
-    UpdateConstantBuffer(ID3D11DeviceContext *context) {
+    UpdateConstantBuffer() {
         D3D11_MAPPED_SUBRESOURCE mapped = {};
 
-        if (FAILED(context->Map(
+        if (FAILED(mDeviceResources->GetDeviceContext()->Map(
                 mBuffer.Get(), 0, D3D11_MAP_WRITE_DISCARD, 0, &mapped))) {
             UtilsFatalError("ERROR: Failed to map constant buffer\n");
         }
         memcpy(mapped.pData, mBytes.data(), mBytes.size());
-        context->Unmap(mBuffer.Get(), 0);
+        mDeviceResources->GetDeviceContext()->Unmap(mBuffer.Get(), 0);
     }
 
     void
-    CreateConstantBuffer(ID3D11Device *device) {
+    CreateConstantBuffer() {
         D3D11_BUFFER_DESC bufferDesc = {};
         bufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
         bufferDesc.ByteWidth = mBytes.size();
@@ -200,14 +204,14 @@ public:
         D3D11_SUBRESOURCE_DATA initData = {};
         initData.pSysMem = mBytes.data();
 
-        if (FAILED(device->CreateBuffer(
+        if (FAILED(mDeviceResources->GetDevice()->CreateBuffer(
                 &bufferDesc, &initData, mBuffer.ReleaseAndGetAddressOf()))) {
             UtilsFatalError(
                 "ERROR: Failed to create per frame constants cbuffer\n");
         }
     }
 
-    const std::vector<uint8_t> &
+    [[nodiscard]] const std::vector<uint8_t> &
     GetBytes() const {
         return mBytes;
     }
