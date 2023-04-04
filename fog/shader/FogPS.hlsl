417f67a58553b3fd0dfe73e4fc2cc4ef64484dde
Nikita Elsakov <vocasle@outlook.com>
2023-04-04 13:41:43 +0300

    Add comments

--- fog/shader/FogPS.hlsl
+++ fog/shader/FogPS.hlsl
@@ -8,8 +8,10 @@ float4 main(PSIn pin) : SV_TARGET
     float4 depth = depthBuffer.Sample(defaultSampler, pin.TexCoord);
 	float4 color =  backBuffer.Sample(defaultSampler, pin.TexCoord);
 
+	// linearize depth values
 	float d = (1.0 - zFar / zNear) * depth.x + (zFar / zNear);
 	d = 1.0 / d;
+	// multiply by far plane
 	d = d * zFar;
 
 	float fogFactor = exp2(-(d * fogDensity) * (d * fogDensity));
