555d322f55ec5de2c849eb9581142c6621b12191
Nikita Elsakov <vocasle@outlook.com>
2023-04-04 15:26:46 +0300

    Change brighness picking

--- bloom/shader/BrightPS.hlsl
+++ bloom/shader/BrightPS.hlsl
@@ -10,9 +10,8 @@ struct VSOut
 float4 main(VSOut pin) : SV_TARGET
 {
     float4 color = colorBuffer.Sample(defaultSampler, pin.TexCoord);
-    static const float threshold = 1.2f;
-    static const float4 mask = float4(0.1f, 0.1f, 0.1f, 1.0f);
-    if (dot(color, mask) < threshold)
+    //float brightness = dot(color.rgb, float3(0.2126, 0.7152, 0.0722));
+    if (length(color.rgb) < 1.0)
     {
         static const float4 black = float4(0, 0, 0, 1);
         return black;
